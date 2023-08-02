85da6e4b66f7b5e424f363d3ce6d8577882878d7
TIotti <meumesmomyself@gmail.com>
2023-08-02 18:30:56 -0300

    create AI service

--- Source/MinderaUE5Project/AI/BTService_TargetLookUp.cpp
+++ Source/MinderaUE5Project/AI/BTService_TargetLookUp.cpp
@@ -1,5 +1,51 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "AI/BTService_TargetLookUp.h"
+#include "../Characters/PlayerCharacter.h"
+#include "Kismet/GameplayStatics.h"
+#include "AIController.h"
+#include "BTService_TargetLookUp.h"
 
+UBTService_TargetLookUp::UBTService_TargetLookUp(const FObjectInitializer& ObjectInitializer) : UBTService(ObjectInitializer)
+{
+	NodeName = "Target lookup";
+	bNotifyBecomeRelevant = true;
+}
+
+
+void UBTService_TargetLookUp::OnSearchStart(FBehaviorTreeSearchData& SearchData)
+{	
+	if (!SearchData.OwnerComp.IsActive())
+		return;
+
+	UBlackboardComponent* BlackboardComp = SearchData.OwnerComp.GetBlackboardComponent();
+	if (!IsValid(BlackboardComp))
+		return;
+	if (BlackboardComp->GetValueAsBool(this->DetectedKey.SelectedKeyName))
+	{		
+		return;
+	}
+	TArray<AActor*> FoundPlayers;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), APlayerCharacter::StaticClass(), FoundPlayers);
+	
+	if (FoundPlayers.Num() > 0)
+	{		
+		BlackboardComp->SetValueAsObject(this->TargetPlayerKey.SelectedKeyName, FoundPlayers[0]);
+	}
+	
+}
+
+void UBTService_TargetLookUp::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	if (!IsValid(BlackboardComp))
+		return;
+	if (BlackboardComp->GetValueAsBool(this->DetectedKey.SelectedKeyName))
+	{
+		AAIController* aiOwner = OwnerComp.GetAIOwner();
+		if (!IsValid(aiOwner))
+			return;
+		aiOwner->StopMovement();
+		return;
+	}
+}
--- Source/MinderaUE5Project/AI/BTService_TargetLookUp.h
+++ Source/MinderaUE5Project/AI/BTService_TargetLookUp.h
@@ -4,14 +4,24 @@
 
 #include "CoreMinimal.h"
 #include "BehaviorTree/Services/BTService_BlackboardBase.h"
+#include "BehaviorTree/BlackboardComponent.h"
 #include "BTService_TargetLookUp.generated.h"
 
 /**
  * 
  */
-UCLASS()
-class MINDERAUE5PROJECT_API UBTService_TargetLookUp : public UBTService_BlackboardBase
+UCLASS(Blueprintable)
+class MINDERAUE5PROJECT_API UBTService_TargetLookUp : public UBTService
 {
 	GENERATED_BODY()
-	
+public:
+	UBTService_TargetLookUp(const FObjectInitializer& ObjectInitializer);
+
+	virtual void OnSearchStart(FBehaviorTreeSearchData& SearchData);
+
+	UPROPERTY(EditAnywhere, Category = Blackboard)
+		FBlackboardKeySelector TargetPlayerKey;
+	UPROPERTY(EditAnywhere, Category = Blackboard)
+		FBlackboardKeySelector DetectedKey;
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
 };
