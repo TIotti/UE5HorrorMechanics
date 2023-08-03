e3d6f0faac11d4f9db7d1e78b3480a69d0087c11
TIotti <meumesmomyself@gmail.com>
2023-08-03 02:09:29 -0300

    Create PlayerPawnSensing

---
+++ Source/MinderaUE5Project/Public/AI/PlayerPawnSensing.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Perception/PawnSensingComponent.h"
+#include "PlayerPawnSensing.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MINDERAUE5PROJECT_API UPlayerPawnSensing : public UPawnSensingComponent
+{
+	GENERATED_BODY()
+	
+};
